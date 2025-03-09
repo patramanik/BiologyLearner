57e0332e9579e42087542a64b84e66c3435e7598
Manik patra <72035800+patramanik@users.noreply.github.com>
2025-03-09 17:45:41 +0530

    add

--- resources/views/layouts/inc/admin-navbar.blade.php
+++ resources/views/layouts/inc/admin-navbar.blade.php
@@ -8,7 +8,7 @@
         <i class="fas fa-bars"></i>
     </button>
     <!-- Navbar Search -->
-    <form class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
+    <!-- <form class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
         <div class="input-group">
             <input class="form-control" type="text" placeholder="Search for..." aria-label="Search for..."
                 aria-describedby="btnNavbarSearch" />
@@ -16,31 +16,31 @@
                 <i class="fas fa-search"></i>
             </button>
         </div>
-    </form>
+    </form> -->
     <!-- Navbar -->
-    <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
-        <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button"
-                data-bs-toggle="dropdown" aria-expanded="false">
-                <i class="fas fa-user fa-fw"></i>
-            </a>
-            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                <li><a class="dropdown-item" href="{{ url('/profile') }}">Profile</a></li>
-                {{-- Uncomment the following lines if needed --}}
-                {{-- <li><a class="dropdown-item" href="#!">Settings</a></li> --}}
-                {{-- <li><a class="dropdown-item" href="#!">Activity Log</a></li> --}}
-                <li>
-                    <hr class="dropdown-divider" />
-                </li>
-                <li>
-                    <form method="POST" action="{{ route('logout') }}">
-                        @csrf
-                        <button type="submit" class="dropdown-item">
-                            {{ __('Log Out') }}
-                        </button>
-                    </form>
-                </li>
-            </ul>
-        </li>
-    </ul>
+      <!-- Right-aligned Navbar Content -->
+    <div class="ms-auto d-flex align-items-center p-2">
+        <ul class="navbar-nav">
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button"
+                    data-bs-toggle="dropdown" aria-expanded="false">
+                    <i class="fas fa-user fa-fw"></i>
+                </a>
+                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                    <li><a class="dropdown-item" href="{{ url('/profile') }}">Profile</a></li>
+                    <li>
+                        <hr class="dropdown-divider">
+                    </li>
+                    <li>
+                        <form method="POST" action="{{ route('logout') }}">
+                            @csrf
+                            <button type="submit" class="dropdown-item">
+                                {{ __('Log Out') }}
+                            </button>
+                        </form>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </div>
 </nav>
